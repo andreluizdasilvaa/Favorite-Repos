261727f7f168b9c0873f25b6d5c04caad150df5a
André <sandreluiz486@gmail.com>
2025-04-21 16:50:06 -0300

    Add axios + base url, animation Loading in button with keyframe

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,53 @@
-import React from 'react';
-import { FaGithub, FaPlus } from 'react-icons/fa';
+import React, { useState, useCallback } from 'react';
+import { FaGithub, FaPlus, FaSpinner } from 'react-icons/fa';
 
-import { Title, Container, Form, SubmitButton } from './styles';
+import {
+    Title,
+    Container,
+    Form,
+    SubmitButton,
+} from './styles';
+import api from '../../services/api';
 
 export default function Repositorio() {
+    const [newRepo, setNewRepo] = useState('');
+    const [repositorios, setRepositorios] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    const handleSubmit = useCallback((e) => {
+        async function submit() {
+            setLoading(true)
+            try {
+                e.preventDefault();
+
+                if(!newRepo) {
+                    return;
+                }
+
+                await new Promise(resolve => setTimeout(resolve, 3000));
+                const response = await api.get(`repos/${newRepo}`);
+
+                const data = {
+                    name: response.data.full_name,
+                };
+
+                setRepositorios([...repositorios, data]);
+                setNewRepo('');
+                console.log(repositorios);
+            } catch (error) {
+                console.log(error);
+            } finally {
+                setLoading(false)
+            }
+        }
+        
+        submit();
+    }, [newRepo, repositorios])
+
+    function handleInputChange(e) {
+        setNewRepo(e.target.value);
+    }
+
     return (
         <Container>
             <h1>
@@ -11,13 +55,25 @@ export default function Repositorio() {
                 Meus repositorios
             </h1>
 
-            <Form onSubmit={() => {}}>
-                <input type='text' placeholder='Adicionar Repositórios' />
+            <Form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Adicionar Repositórios"
+                    value={newRepo}
+                    onChange={handleInputChange}
+                />
 
-                <SubmitButton>
-                    <FaPlus color='#fff' size={14} />
+                <SubmitButton loading={loading ? 1 : 0}>
+                    {
+                    loading ? (
+                            <FaSpinner size={14} color='#fff' />
+                        ) : (
+                            <FaPlus color="#fff" size={14} />
+                        )
+                    }
+                    
                 </SubmitButton>
             </Form>
         </Container>
-    )
+    );
 }
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
 import { PrimaryColor } from '../../styles/colors'
 
 export const Title = styled.h1`
@@ -39,7 +39,21 @@ export const Form = styled.form`
     }
 `;
 
-export const SubmitButton = styled.button`
+// Animação spinner no button
+const animateSpinner = keyframes`
+    from{
+        transform: rotate(0deg);
+    }
+
+    to{
+        transform: rotate(360deg);
+    }
+`;
+
+export const SubmitButton = styled.button.attrs(props => ({
+    type: 'submit',
+    disabled: props.loading,
+}))`
     background-color: ${PrimaryColor};
     border: 0;
     border-radius: 4px;
@@ -48,4 +62,17 @@ export const SubmitButton = styled.button`
     display: flex;
     justify-content: center;
     align-items: center;
+
+    &[disabled] {
+        cursor: not-allowed;
+        opacity: 0.5;
+    }
+
+    ${props => props.loading &&
+    css`
+        svg {
+            animation: ${animateSpinner} 2s linear infinite;
+        }
+    `
+    }
 `;
